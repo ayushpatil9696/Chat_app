9423e8bae3e173882b629650b5e52e5eafe90c7f
Ayush patil <patil.ayush1227@gmail.com>
2025-05-12 21:48:29 +0530

    5th commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRouter from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.get("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on port: " + PORT);
